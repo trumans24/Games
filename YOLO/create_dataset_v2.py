defc7b1a1d7cdd2ba40623f0cae433b4055d48de
Spencer Truman <trumans24@gmail.com>
2024-06-12 23:06:23 +0300

    Improved dataset augmentation

---
+++ YOLO/create_dataset_v2.py
@@ -0,0 +1,80 @@
+"""
+python create_dataset_v2.py /home/trumanss/CodeProject/Games/data/playing_cards -d dataset/object_detection_v2/ --train 10000 --val 3000
+"""
+import argparse
+from tqdm import trange
+from util import AddBackground
+from random import choice, randint
+from pathlib import Path
+from torch.nn import Module
+from torchvision.datasets import ImageFolder, FakeData
+from torchvision.transforms.v2 import Resize, Compose, RandomResize, ColorJitter, RandomRotation, RandomPerspective
+
+# Initialize parser
+parser = argparse.ArgumentParser(prog='test', description='Testing functionality of argparse', epilog='I am exploring argparse and I need to understand what is happening')
+parser.add_argument('source', help="Specify the root of the directory with images of cards and which must follow pytorch's convention for ImageFolder.")
+parser.add_argument('-d', '--destination', default='dataset/object_detection/', help="Specify the root of the directory where the dataset should be saved.")
+parser.add_argument('--train', type=int, default=100, help="Specify how many sample to make for the training dataset")
+parser.add_argument('--val', type=int, default=10, help="Specify how many sample to make for the validation dataset")
+parser.add_argument('--test', type=int, default=0, help="Specify how many sample to make for the testing dataset")
+
+# Read arguments from command line
+args = parser.parse_args()
+
+card_transform = Compose([
+    RandomResize(50, 112),
+    ColorJitter(brightness=(0.5, 1.0), contrast=(0.5, 1.0)),
+    RandomRotation(30, expand=True),
+    RandomPerspective(0.3),
+])
+
+cards = ImageFolder(args.source, transform=card_transform)
+dst = Path(args.destination)
+
+resize = Resize(112)
+combine_cards = AddBackground(448, 448)
+
+for group in [g for g in ['train', 'val', 'test'] if vars(args)[g] > 0]:
+    images_dir = dst / 'images' / group
+    labels_dir = dst / 'labels' / group
+    
+    if not images_dir.is_dir():
+        images_dir.mkdir(parents=True)
+    if not labels_dir.is_dir():
+        labels_dir.mkdir(parents=True)
+
+    for i in trange(vars(args)[group]):
+        card_sample = []
+        num_cards = randint(1, 3)
+        for _ in range(num_cards):
+            img, label = choice(cards)
+            img = resize(img)
+            card_sample.append((img, label))
+        img, label = combine_cards(card_sample)
+        img_file = images_dir / f'img{str(i).rjust(9, '0')}.jpg'
+        label_file = labels_dir / f'img{str(i).rjust(9, '0')}.txt'
+        img.save(img_file, "JPEG")
+        img.close()
+        with open(label_file, 'w') as f:
+            f.write(label)
+
+import yaml
+
+if args.test:
+    data = {
+        'path':f'../{dst.relative_to(dst.parents[1])}',
+        'train': 'images/train',
+        'val': 'images/val',
+        'test': 'images/test',
+        'names': dict(zip(range(len(cards.classes)), cards.classes))
+        }
+else:
+    data = {
+        'path':f'../{dst.relative_to(dst.parents[1])}',
+        'train': 'images/train',
+        'val': 'images/val',
+        'names': dict(zip(range(len(cards.classes)), cards.classes))
+        }
+    
+with open(dst/'data.yaml', 'w') as outfile:
+    yaml.dump(data, outfile, default_flow_style=False)
--- YOLO/util.py
+++ YOLO/util.py
@@ -1,12 +1,16 @@
-from torch import randint as randi, uint8
+from torch import randint as randi, uint8, zeros_like
 from random import choice, randint
 from torch.nn import Module
-from torchvision.datasets import FakeData
-from torchvision.transforms.functional import to_pil_image
+from torchvision.transforms.functional import to_pil_image, pil_to_tensor
+
+def fake_image(height, width, channel=3, PIL=True):
+    if PIL:
+        return to_pil_image(randi(0, 255, (channel, height, width), dtype=uint8))
+    return randi(0, 255, (channel, height, width), dtype=uint8)
 
 class CombineImages(Module):
     def forward(self, imgs):
-        background = to_pil_image(randi(0, 255, (3, 224, 224), dtype=uint8))
+        background = fake_image(224, 224)
         for img in imgs:
             x = randint(0, background.width - img.width)
             y = randint(0, background.height - img.height)
@@ -15,7 +19,7 @@ class CombineImages(Module):
 
 class CombineCards(Module):
     def forward(self, samples):
-        background = to_pil_image(randi(0, 255, (3, 224, 224), dtype=uint8))
+        background = fake_image(224, 224)
         labels = []
         for img, label in samples:
             x = randint(0, background.width - img.width)
@@ -24,4 +28,30 @@ class CombineCards(Module):
             cy = (y + img.height // 2) / background.height
             labels.append(f'{label} {cx} {cy} {img.width / background.width} {img.height / background.height}')  # class cx cy width height
             background.paste(img, (x, y))
+        return background, '\n'.join(labels)
+
+class AddBackground(Module):
+    def __init__(self, width=448, height=448, PIL=True) -> None:
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.PIL = PIL
+
+    def forward(self, samples):
+        background = fake_image(self.height, self.width, PIL=False)
+        labels = []
+        for img, label in samples:
+            height, width = img.height, img.width
+            x = randint(0, self.width-width)
+            y = randint(0, self.height-height)
+            img = pil_to_tensor(img)
+            mask = zeros_like(background)
+            mask[:, y:y+height, x:x+width] = img
+            background[:, (mask >  0).any(0)] = 0
+            background = mask | background
+            cx = (x + width // 2) / self.width
+            cy = (y + height // 2) / self.height
+            labels.append(f'{label} {cx} {cy} {width / self.width} {height / self.height}')  # class cx cy width height
+        if self.PIL:
+            return to_pil_image(background), '\n'.join(labels)
         return background, '\n'.join(labels)
